34ab3a3b02a6e9ab717ad31559e0617af999b65e
SunOfLife1 <yfitzge@gmail.com>
2025-03-31 15:10:22 -0400

    Add barebones madlibs activity

---
+++ activity1_madlibs.py
@@ -0,0 +1,9 @@
+# This is a comment! Anything in a comment is ignored when running the code
+
+# Ask the user for input
+noun = input("Enter a noun: ")
+verb = input("Enter a verb ending in 'ing': ")
+
+# Print the resulting madlib
+print(noun + " is " + verb)
+
