742c09b48dc87b55fafde5bf37772cc31788efd3
SunOfLife1 <yfitzge@gmail.com>
2025-04-02 15:24:04 -0400

    Activity 1: Change the sentence to something interesting

--- activity1_madlibs.py
+++ activity1_madlibs.py
@@ -11,9 +11,12 @@ print("I'm going to ask you for some words, and then I'll put them into a senten
 
 # The "input" function displays text, then waits for the user to input something.
 # By using something like "word = input("Some text")", we're saving whatever the user typed to a variable called "word".
-noun = input("Enter a noun: ")
-verb = input("Enter a verb ending in 'ing': ")
+noun      = input("Enter a noun: ")
+verb_ing  = input("Enter a verb ending in 'ing': ")
+adjective = input("Enter an adjective: ")
+verb_ed   = input("Enter a verb ending in 'ed': ")
 
 # This uses a print statement to display the full sentence.
 # The usage of "+" combines text with our variables, so we can display sentences with the user-inputted text mixed in.
-print(noun + " is " + verb)
+print("Today I saw a " + noun + " that was " + verb_ing + ". " +
+      "It was so " + adjective + " that I almost " + verb_ed + ".")
