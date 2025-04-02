c983d4444a2911bf037ac4a180cf92dd58663a97
SunOfLife1 <yfitzge@gmail.com>
2025-04-02 15:56:39 -0400

    Activity 2 (Simple): Print before exiting

--- activity2_calculator_simple.py
+++ activity2_calculator_simple.py
@@ -4,16 +4,19 @@ print("This program is a calculator. Enter two numbers and an operation to perfo
 num1 = input("First Number to calculate: ")
 
 if not num1.isdigit():
+    print("That's not a whole number. Exiting now..")
     exit(1)
 
 num2 = input("Second Number to calculate: ")
 
 if not num2.isdigit():
+    print("That's not a whole number. Exiting now..")
     exit(1)
 
 operation = input("Operation: ")
 
 while operation not in ("+", "-", "/", "*"):
+    print("That operation isn't supported. Exiting now..")
     exit(1)
 
 num1 = int(num1)
