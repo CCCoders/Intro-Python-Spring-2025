99cae0ffdc1338a58f9b8e3b5768735326cdf135
SunOfLife1 <yfitzge@gmail.com>
2025-04-02 15:46:46 -0400

    Activity 2 (Simple): Remove extra whitespace

--- activity2_calculator_simple.py
+++ activity2_calculator_simple.py
@@ -10,12 +10,12 @@ num2 = input("Second Number to calculate: ")
 
 if not num2.isdigit():
     exit(1)
-    
+
 operation = input("Operation: ")
 
 while operation not in ("+", "-", "/", "*"):
     exit(1)
-    
+
 num1 = int(num1)
 num2 = int(num2)
 
