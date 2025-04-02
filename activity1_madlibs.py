fe83d2d7c6082248cf19ba7e7f834954206e4e71
SunOfLife1 <yfitzge@gmail.com>
2025-04-02 15:20:47 -0400

    Activity 1: Explain more in comments

--- activity1_madlibs.py
+++ activity1_madlibs.py
@@ -1,8 +1,10 @@
-# This is a comment! Anything in a comment is ignored when running the code
+# This is a comment! Anything in a comment is ignored when running your code.
 
-# Ask the user for input
+# The "input" function displays text, then waits for the user to input something.
+# By using something like "word = input("Some text")", we're saving whatever the user typed to a variable called "word".
 noun = input("Enter a noun: ")
 verb = input("Enter a verb ending in 'ing': ")
 
-# Print the resulting madlib
+# This uses a print statement to display the full sentence.
+# The usage of "+" combines text with our variables, so we can display sentences with the user-inputted text mixed in.
 print(noun + " is " + verb)
