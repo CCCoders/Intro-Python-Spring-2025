41b2e07fffbad4137f5e5dfd1b54aa4ab392a8bf
SunOfLife1 <yfitzge@gmail.com>
2025-04-02 20:15:02 -0400

    Activity 2 (Simple): Move string-to-int conversions up
    - Moves the conversions of num1 and num2 from string to int up to where they're read

--- activity2_calculator_simple.py
+++ activity2_calculator_simple.py
@@ -7,20 +7,21 @@ if not num1.isdigit():
     print("That's not a whole number. Exiting now..")
     exit(1)
 
+num1 = int(num1)
+
 num2 = input("Second Number to calculate: ")
 
 if not num2.isdigit():
     print("That's not a whole number. Exiting now..")
     exit(1)
 
+num2 = int(num2)
+
 operation = input("Operation: ")
 
 while operation not in ("+", "-", "/", "*"):
     print("That operation isn't supported. Exiting now..")
     exit(1)
-
-num1 = int(num1)
-num2 = int(num2)
 
 if operation == "+":
     print(num1 + num2)
