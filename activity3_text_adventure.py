8f152e0bf8e40a4fca53f16cd0ceacf9b9b7f2e4
SunOfLife1 <yfitzge@gmail.com>
2025-04-02 22:20:18 -0400

    Activity 3: Change camelCase to snake_case

--- activity3_text_adventure.py
+++ activity3_text_adventure.py
@@ -10,7 +10,7 @@ Last Updated: Apr 02, 2025
 # Anything within the method will happen whenever the method is called
 
 
-def openingTalk():
+def opening_talk():
     print("Hello traveller!")
 
     name = input("What is your name?\n")
@@ -18,7 +18,7 @@ def openingTalk():
     print(name + ", I hope you're ready to go on an adventure!")
 
 
-def ghostTown():
+def ghost_town():
     print(
         "\nTobey: Welcome to the town of Blanchester! We haven't had visitors"
         + " in fifty years. Why do you come here?"
@@ -26,54 +26,54 @@ def ghostTown():
 
     questioning = True
     while questioning:
-        whyComeHere = input(
+        why_come_here = input(
             "\n1: To steal all your gold!\n2: To help get rid"
             + " of your scary ghosts.\n3: I just wanted to learn your history!"
         )
-        if whyComeHere == "1":
+        if why_come_here == "1":
             print("\nTobey: We don't have anything for you! Gone get!")
             questioning = False
-        elif whyComeHere == "2":
-            ghostHunting()
+        elif why_come_here == "2":
+            ghost_hunting()
             questioning = False
-        elif whyComeHere == "3":
-            historyOfGhostTown()
+        elif why_come_here == "3":
+            history_of_ghost_town()
             questioning = False
         else:
             print("Invalid Input: Try again")
             questioning = True
 
 
-def ghostHunting():
+def ghost_hunting():
     print(
         "\n**********************************"
         + "\nGhost hunting chosen... Continue the story from here"
     )
 
 
-def historyOfGhostTown():
+def history_of_ghost_town():
     print(
         "\n**********************************"
         + "\nHistory of town chosen... Continue the story from here"
     )
 
 
-openingTalk()
+opening_talk()
 
 print("\nAre you ready to go to the ghost town?")
 
 # A variable an a while loop can be used to ask for specific values only
 asking = True
 while asking:
-    adventureStart = input("Ghost Town: G\nExit Game: E\n")
+    adventure_start = input("Ghost Town: G\nExit Game: E\n")
 
     # After asking the user if they want to go to the ghost town or exit
-    # the program will either run the ghostTown() method or exit the program
+    # the program will either run the ghost_town() method or exit the program
 
-    if adventureStart == "G" or adventureStart == "g":
+    if adventure_start == "G" or adventure_start == "g":
         asking = False
         ghost_town()
-    elif adventureStart == "E" or adventureStart == "e":
+    elif adventure_start == "E" or adventure_start == "e":
         asking = False
         exit()
     else:
